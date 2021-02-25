e478601d12dcf573746a40d8fac4e4074c63b2ba
kor <kor.dwarshuis@gmail.com>
2021-02-25 09:21:27 +0100

    Removed comments

--- tweets-quickstart.php
+++ tweets-quickstart.php
@@ -1,6 +1,5 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("content-type: application/json");
-    // echo file_get_contents("https://blockchainbird.com/t/twitter-phirehose/tweets-quickstart.json");
     echo file_get_contents("./tweets-quickstart.json");
 ?>
